938b68f7b956d26ca3bbd9b17aba5a06b3001361
Åsa Johansson <asa.j.1989@gmail.com>
2025-01-30 11:39:22 +0100

    IBaseRepository and BaseRepository created

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    // Create
+    Task<TEntity> CreateAsync(TEntity entity);
+
+    //Read
+    Task<ICollection<TEntity>> GetAllAsync();
+    Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> expression);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+
+    //Update
+    Task<bool> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity);
+
+    //Delete
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,112 @@
+﻿using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity>(DbContext context) : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly DbContext _context = context;
+
+    public async Task<TEntity> CreateAsync(TEntity entity)
+    {
+        if (entity == null) return null!;
+        try
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        catch(Exception ex)
+        {
+            Debug.WriteLine($"Error creating customer {ex.Message}");
+            return null!;
+        }
+    }  
+
+    public async Task<ICollection<TEntity>> GetAllAsync()
+    {
+        try
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+        catch(Exception ex)
+        {
+            Debug.WriteLine($"Error getting list from database {ex.Message}");
+            return [];
+        }
+    }
+
+    public async Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+            var entity = await _context.Set<TEntity>()
+                .FirstOrDefaultAsync(expression);
+
+            if (entity == null) return null!;
+
+            return entity;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error finding entity in database {ex.Message}");
+            return null!;
+        }
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+            return await _context.Set<TEntity>()
+                .AnyAsync(expression);
+        }
+        catch(Exception ex)
+        {
+            Debug.WriteLine($"Error checking if entity exists {ex.Message}");
+            return false;
+        }
+    }
+
+    public async Task<bool> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity)
+    {
+        var entityToUpdate = await _context.Set<TEntity>()
+            .FirstOrDefaultAsync(expression);
+        if (entityToUpdate == null) return false;
+
+        try
+        {
+            _context.Entry(entityToUpdate)
+                .CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error updating entity { ex.Message }");
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        try
+        {
+            var entity = await _context.Set<TEntity>()
+                .FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+
+            if (entity == null) return false;
+
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error deleting entity { ex.Message }");
+            return false;
+        }
+    }
+}
